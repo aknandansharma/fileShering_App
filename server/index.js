170fbc97e09411479087bdc47ff6440626cf6d13
Aknandan Sharma <90132050+aknandansharma@users.noreply.github.com>
2023-08-03 16:25:46 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './routes/router.js';
 import cors from 'cors'
 import DBConnection from './dataBase/db.js';
+import path from 'path'
 
 
 // inti
@@ -12,7 +13,12 @@ app.use(cors());
 
 // create route
 app.use('/', router)
+app.use(express.static(path.join(__dirname, '../client/build')))
 
+// function to get all data from database
+app.use('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // database
 DBConnection();
